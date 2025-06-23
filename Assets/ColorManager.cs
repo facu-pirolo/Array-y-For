1a06e3c6a430c60b3ed263efd1494a93f15c7863
facu-pirolo <49518308@est.ort.edu.ar>
2025-06-23 10:22:31 -0300

    hola

---
+++ Assets/ColorManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorManager : MonoBehaviour
+{
+    public Color[] colors;
+    public Renderer cubeRenderer;
+    public int randomColorIndex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating(nameof(ChangeColor), 0, 0.5f);
+       
+        
+    }
+
+    void ChangeColor()
+    {
+        randomColorIndex = Random.Range(0, colors.Length);
+        cubeRenderer.material.color = colors[randomColorIndex];
+    }
+}
